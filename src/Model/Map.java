01f5690a7893d28c7ab2240764d652ad2e698c57
tigier <tigierlp@gmail.com>
2016-09-20 22:21:34 +0200

    alles muss man selber machen

--- src/Model/Map.java
+++ src/Model/Map.java
@@ -9,22 +9,22 @@ public class Map {
         mapArray[0][0]=new Tile(0,0,0);
     }
 
-    //TODO ergänze tile gneration
+    //TODO contents
     public void extendArray(int w){
-        if(w == 0){
-        }
-        else if(w == 1) {
+        if(w == 0) {
             Tile[][] dummy = new Tile[mapArray.length + 1][mapArray[0].length];
             System.arraycopy(mapArray, 0, dummy, 0, mapArray.length);
             System.arraycopy(mapArray[0], 0, dummy[0], 0, mapArray[0].length);
             mapArray = dummy;
+            mapArray[player.getYPos()][player.getXPos()] = new Tile(player.getXPos(),player.getYPos(),0);
             System.out.print(mapArray.length);
         }
-        else if (w == 2){
+        else if (w == 1){
             Tile[][] dummy = new Tile[mapArray.length][mapArray[1].length + 1];
             System.arraycopy(mapArray,0,dummy,0,mapArray.length);
             System.arraycopy(mapArray[1],0,dummy[1],0,mapArray[1].length);
             mapArray = dummy;
+            mapArray[player.getYPos()][player.getXPos()] = new Tile(player.getXPos(),player.getYPos(),0);
             System.out.print(mapArray[1].length);
         }
     }
